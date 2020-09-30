e759ceb9bdae75df3b1f3cbbdc24b0aea0bb5b25
Aidan Furey <aidan.t.furey@gmail.com>
2020-09-30 12:50:45 -0400

    Part 2 update

---
+++ myApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  myApp
+//
+//  Created by Aidan Furey on 9/30/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
